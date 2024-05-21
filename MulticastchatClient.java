7a5caf532ee1776ed5b66555fb8e9256758f6b8e
Eris Kociaj <eris.kociaj@elev.ga.ntig.se>
2024-05-21 13:02:58 +0200

    2#: Modified the portnumber variable to match the port used by the server (4446). Adjusted the condition in the if statement to check if at least one argument is provided (args.length > 0).

--- MulticastchatClient.java
+++ MulticastchatClient.java
@@ -5,10 +5,10 @@ public class MulticastchatClient {
 
     public static void main(String args[]) throws Exception {
 
-        // Default port number we are going to use 
-        int portnumber = 5000;
+        // Default port number we are going to use
+        int portnumber = 4446; // Changed default port number to match the server
 
-        if (args.length > 1)
+        if (args.length > 0) // Changed condition to check if at least one argument is provided
             portnumber = Integer.parseInt(args[0]);
 
         // Create a MulticastSocket
@@ -17,17 +17,22 @@ public class MulticastchatClient {
         // Determine the IP address of a host, given the host name
         InetAddress group = InetAddress.getByName("225.4.5.6");
 
-        // Joins a multicast group
-        chatMulticastSocket.joinGroup(group);
+        // Create a SocketAddress from the IP address and port number
+        InetSocketAddress groupAddress = new InetSocketAddress(group, portnumber);
 
-        // Prompt a user to enter a message
-        String msg = "";
+        // Obtain a network interface for multicast
+        NetworkInterface networkInterface = NetworkInterface.getNetworkInterfaces().nextElement(); // Adjusted to match server's approach
 
-        System.out.println("Type a message for the server: "); 
+        // Joins a multicast group using the new method
+        chatMulticastSocket.joinGroup(groupAddress, networkInterface);
+
+        // Clear the console or add a visual separator before the prompt
+        System.out.println("\n-------------------------------------------------");
+        System.out.println("Type a message for the server: ");
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        msg = br.readLine();
+        String msg = br.readLine();
 
         // Send the message to Multicast address
         DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
